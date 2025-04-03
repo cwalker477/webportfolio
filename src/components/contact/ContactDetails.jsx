264860e446a564ea99aedba37340685b3208d8e3
Clayton Walker <clayton@walkervfx.com>
2025-04-02 20:48:36 -0400

    fix: update contact details

--- src/components/contact/ContactDetails.jsx
+++ src/components/contact/ContactDetails.jsx
@@ -3,19 +3,10 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'St Petersburg, FL', // Updated address
 		icon: <FiMapPin />,
 	},
-	{
-		id: 2,
-		name: 'email@domain.com',
-		icon: <FiMail />,
-	},
-	{
-		id: 3,
-		name: '555 8888 888',
-		icon: <FiPhone />,
-	},
+	// Removed email and phone objects
 ];
 
 const ContactDetails = () => {
